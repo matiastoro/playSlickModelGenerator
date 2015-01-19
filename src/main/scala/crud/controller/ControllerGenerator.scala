70a35c799f8e3f3bd662df084cb6dc37b12c4693
Matías Toro Ipinza <mtoro.cl@gmail.com>
2015-01-19 00:25:35 -0300

    Controllers are finished

--- src/main/scala/crud/controller/ControllerGenerator.scala
+++ src/main/scala/crud/controller/ControllerGenerator.scala
@@ -12,7 +12,7 @@ import via56.slickGenerator.Table
 case class ControllerGenerator(table: Table) extends CodeGenerator{
   def generate: String = {
     val objectSignature = """object """+table.className+"""Controller extends ApplicationController {"""
-    val l = List(imports, objectSignature, index(), show(), form())
+    val l = List(imports, objectSignature, index(), show(), form(), create, save, edit, update)
     println(table.columns)
     l.mkString("\n")
   }
@@ -35,7 +35,9 @@ import org.joda.time.{DateTimeZone, DateTime}
 import play.api.i18n.Messages"""
 
   def index(): String = {
+
     """
+/*"""+routes+"""*/
   def index(page: Int = 1, pageLength: Int = 20) = Action { implicit request =>
     val pagination = """+table.queryName+""".paginate("""+table.queryName+""".allQuery,pageLength,page)
     Ok(views.html."""+table.viewsPackage+""".index(pagination.results, pagination.count, page, pageLength))
@@ -82,7 +84,64 @@ import play.api.i18n.Messages"""
   )"""
   }
 
+  def edit(): String = {
+    """
+  def edit(id: Long) = Action{ implicit request =>
+    """+table.queryName+""".byId(id).map{ """+table.objName+""" =>
 
+      Ok(views.html."""+table.viewsPackage+""".edit(form.fill("""+table.objName+"""), """+table.objName+"""))
+    }.getOrElse(NotFound)
+  }"""
+  }
 
+  def create(): String = {
+  """
+  def create() = Action{ implicit request =>
+    Ok(views.html."""+table.viewsPackage+""".create(form))
 
+  }"""
+  }
+
+  def update = {
+  """
+  def update(id: Long) = Action{ implicit request =>
+    """+table.queryName+""".byId(id).map{ """+table.objName+""" =>
+      form.bindFromRequest.fold(
+        formWithErrors => BadRequest(views.html."""+table.viewsPackage+""".edit(formWithErrors, """+table.objName+""")),
+        obj => {
+          """+table.queryName+""".update(obj.copy(id = """+table.objName+""".id)).map{ id =>
+            Redirect(routes."""+table.className+"""Controller.show("""+table.objName+""".id.get)).flashing("success" -> Messages("save.success"))
+          }.getOrElse(NotFound)
+        }
+      )
+    }.getOrElse(NotFound)
+  }"""
+  }
+  def save = {
+    """
+  def save() = Action{ implicit request =>
+
+    form.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html."""+table.viewsPackage+""".create(formWithErrors)),
+      obj => {
+        val id = """+table.queryName+""".insert(obj)
+        Redirect(routes."""+table.className+"""Controller.show(id)).flashing("success" -> Messages("save.success"))
+      }
+    )
+  }
+  """
+  }
+  
+  def routes = {
+    """
+GET         /"""+table.objName+"""/                  controllers."""+table.className+"""Controller.index(page: Int = 1, pageLength: Int = 20)
+GET         /"""+table.objName+"""/show/:id          controllers."""+table.className+"""Controller.show(id: Long)
+GET         /"""+table.objName+"""/edit/:id          controllers."""+table.className+"""Controller.edit(id: Long)
+GET         /"""+table.objName+"""/create            controllers."""+table.className+"""Controller.create()
+POST        /"""+table.objName+"""/save              controllers."""+table.className+"""Controller.save()
+POST        /"""+table.objName+"""/update/:id        controllers."""+table.className+"""Controller.update(id: Long)
+GET         /"""+table.objName+"""/:page/:pageLength controllers."""+table.className+"""Controller.index(page: Int, pageLength: Int)
+GET         /"""+table.objName+"""/:page             controllers."""+table.className+"""Controller.index(page: Int, pageLength: Int = 20)
+"""
+  }
 }
