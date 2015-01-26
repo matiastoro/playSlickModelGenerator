22b2aa0c23ef691820162567d48b1e5140b28b59
Matías Toro Ipinza <mtoro.cl@gmail.com>
2015-01-26 11:50:56 -0300

    Global

---
+++ output/Global.scala
@@ -0,0 +1,34 @@
+
+import actors.Statictitian
+import akka.actor.Props
+import play.api.libs.concurrent.Akka
+
+import play.api._
+
+import play.api.Application
+
+import play.api.Play.current
+import models.db
+
+
+
+
+object Global  extends GlobalSettings {
+
+
+  override def onStart(app: Application) {
+    println("Starting Application")
+    startActor
+  }
+
+  override def onStop(app: Application) {
+    println("Stopping Application")
+  }
+
+
+  lazy val startActor = {
+    db.createDB
+    Akka.system.actorOf(Props[Statictitian], name = "statictitian")
+  }
+
+}
